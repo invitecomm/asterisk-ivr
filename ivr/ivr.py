1ba75a0aa911552c112de213c03529d2d0be3e63
poing <brian.lavallee@invite-comm.jp>
2016-09-12 18:02:18 +0900

    Test

--- ivr/ivr.py
+++ ivr/ivr.py
@@ -20,8 +20,7 @@
 
 class IVR:
     def __init__(self):
-    ''' Constructor for this class. '''
-    # Create some member animals
+        # Create some member animals
         self.members = ['Tiger', 'Elephant', 'Wild Cat']
  
  
