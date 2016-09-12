5d750659f69352fd87cdea1e8abdcb7962ae153c
poing <brian.lavallee@invite-comm.jp>
2016-09-12 17:51:26 +0900

    Test

--- ivr/ivr.py
+++ ivr/ivr.py
@@ -20,12 +20,12 @@
 
 class Mammals:
     def __init__(self):
-        ''' Constructor for this class. '''
-        # Create some member animals
+    ''' Constructor for this class. '''
+    # Create some member animals
         self.members = ['Tiger', 'Elephant', 'Wild Cat']
  
  
     def printMembers(self):
         print('Printing members of the Mammals class')
         for member in self.members:
-            print('\t%s ' % member)
+        print('\t%s ' % member)
