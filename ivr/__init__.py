e1630cc9d7146da2aaf415e72a83916a6154d756
poing <brian.lavallee@invite-comm.jp>
2016-09-12 18:09:27 +0900

    Test

--- ivr/__init__.py
+++ ivr/__init__.py
@@ -1,3 +1,18 @@
+""" ivr - A set of IVR functions for Asterisk.
+
+The pyst project includes several python modules to assist in programming
+asterisk with python:
+
+ivr     - The IVR Template
+
+"""
+
+#from ivr.ivr import *
+
+#__all__ = ['ivr']
+__version__ = '0.0.2'
+
+
 def joke():
     return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
             u'Beiherhund das Oder die Flipperwaldt gersput.')
