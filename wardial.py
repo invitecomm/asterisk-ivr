8a88858527a0f3cfef1a3e560a76e313491db14e
poing <brian.lavallee@invite-comm.jp>
2016-09-13 18:05:09 +0900

    wardial testing

--- wardial.py
+++ wardial.py
@@ -87,10 +87,10 @@ data_insert(agi.env['agi_callerid'],agi.env['agi_extension'])
 #agi.stream_file('wardial/greeting')
 
 q1 = question('wardial/question1', '12')
-data_insert('q1',q1)
+#data_insert('q1',q1)
 
 q2 = question('wardial/question2', '123')
-data_insert('q2',q2)
+#data_insert('q2',q2)
 
 q3 = question('wardial/question3', '12345')
 q4 = question('wardial/question4', '123')
