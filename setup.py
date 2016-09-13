94e1d1ddf8f3235aace89b6b9d0cf014a088a7a7
poing <brian.lavallee@invite-comm.jp>
2016-09-13 13:01:17 +0900

    Learning to package

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set et sw=4 fenc=utf-8:
+#
+# Copyright 2016 INVITE Communications Co., Ltd. All Rights Reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 """Packaging files and information."""
 
 try:
@@ -21,7 +42,7 @@ setup(
         'Development Status :: 1 - Planning',
         'Development Status :: 2 - Pre-Alpha',
     ],
-    license = 'GNU GENERAL PUBLIC LICENSE',
+    license = 'GNU General Public License',
 
     # Package dependencies:
     install_requires = ['six>=1.9.0','google-api-python-client==1.5.3','pyst2'],
