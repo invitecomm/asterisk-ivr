f8e9c20166904f23aa4f4d45ae16a8ed982db6b8
poing <brian.lavallee@invite-comm.jp>
2016-09-14 13:22:16 +0900

    DB Work

--- wardial.py
+++ wardial.py
@@ -36,21 +36,17 @@ import mysql.connector as mariadb
 def question(file, valid_digits):
     regexp = re.compile(r'[' + valid_digits + ']')
     
-    #res = ''
-    #loop = 1
-    
-    #while regexp.search(res) is None:
     res = agi.get_data(file, 20000, 1)
-        
-    #    if not res:
-    #        agi.hangup()
-    #    loop = loop + 1
-    #    if loop > 1:
-    #        agi.hangup()
     
     if regexp.search(res) is not None:
-        #agi.verbose('Entry Was %s' % res)
         return res
+
+    res = agi.get_data(file, 20000, 1)
+    
+    if regexp.search(res) is not None:
+        return res
+    if not res:
+        agi.hangup()
 
 
 settings = ConfigParser.RawConfigParser()
