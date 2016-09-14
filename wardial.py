bf231b15061f670d10f13a2d9751a57e7caad5af
poing <brian.lavallee@invite-comm.jp>
2016-09-14 15:36:44 +0900

    DB Work

--- wardial.py
+++ wardial.py
@@ -88,7 +88,7 @@ clid = agi.env['agi_callerid']
 
 
 q1 = question('wardial/question1', '12')
-session_id = data_insert(db_insert % (clid, 'q1', q1))
+session_id = data_insert(db_insert % ('q1', clid, q1))
 agi.verbose('RECORD #%s INSERTED' % session_id)
 
 q2 = question('wardial/question2', '123')
