5def918d390106cf1307230ad48e85faf145b56d
poing <brian.lavallee@invite-comm.jp>
2016-10-25 12:09:21 +0900

    Testing Japanese and DB

--- pigeonhole/database_jp.py
+++ pigeonhole/database_jp.py
@@ -18,8 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-"""AGI script that renders speech to text using Google Cloud Speech API
- using the REST API."""
+"""Touble with Japanese in the DB.  Testing for a solution."""
 
 # [START import_libraries]
 from __future__ import print_function
