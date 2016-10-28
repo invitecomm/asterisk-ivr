c03e5d8194a173f6361816285215494430f5f0ea
poing <brian.lavallee@invite-comm.jp>
2016-10-28 16:53:07 +0900

    Fix bad upload

--- pigeonhole/cust2.py
+++ pigeonhole/cust2.py
@@ -89,7 +89,7 @@ clid = agi.env['agi_accountcode']
 #SIPCALLID=1583cd9c69daeca70f5a91477e22f3b7@172.17.70.223:5060
 
 wombat = agi.get_variable('WOMBAT_HOPPER_ID')
-warlist = agi.env('agi_accountcode')
+#warlist = agi.get_variable('agi_accountcode')
 newTable = agi.get_variable('table')
 
 
@@ -99,6 +99,8 @@ agi.verbose("Database Record: {0}".format(warlist))
 
 amdstatus = agi.env['agi_arg_2']
 amdreason = agi.env['agi_arg_3']
+warlist = agi.env['agi_accountcode']
+
 
 if amdstatus == "MACHINE":
     agi.appexec('UserEvent', 'CALLSTATUS, UniqueID:%s,V:AMD' % wombat)
