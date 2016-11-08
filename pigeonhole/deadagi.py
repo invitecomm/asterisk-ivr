4b5b90dc05e8dc164dbdc21b6a2f0da584df7656
poing <brian.lavallee@invite-comm.jp>
2016-11-08 17:26:19 +0900

    DeadAGI testing

--- pigeonhole/deadagi.py
+++ pigeonhole/deadagi.py
@@ -25,124 +25,12 @@
 from __future__ import print_function
 
 from asterisk.agi import *
-import re
-import ConfigParser
-
-from datetime import date, datetime, timedelta
-import mysql.connector as mariadb
-
-def question(file, valid_digits):
-    regexp = re.compile(r'[' + valid_digits + ']')
-    
-    res = agi.get_data(file, 20000, 1)
-    if regexp.search(res) is not None:
-        return res
-
-    res = agi.get_data(file, 20000, 1)    
-    if regexp.search(res) is not None:
-        return res
-        
-    if not res:
-        agi.hangup()
-
-settings = ConfigParser.RawConfigParser()
-settings.read('/etc/asterisk/res_config_mysql.conf')
-
-config = {
-  'user': settings.get('general', 'dbuser'),
-  'password': settings.get('general', 'dbpass'),
-  'host': settings.get('general', 'dbhost'),
-  'database': 'wardial',
-  'raise_on_warnings': True,
-}
-
-#  'database': settings.get('general', 'dbname'),
-
-
-def data_insert(query):                           
-    agi.verbose(query)
-    try:
-        mariadb_connection = mariadb.connect(**config)
-        cursor = mariadb_connection.cursor()
-        cursor.execute(query)
-        record = cursor.lastrowid
-        mariadb_connection.commit()
-        cursor.close()
-        mariadb_connection.close()
-    except mariadb.Error as error:
-        agi.verbose("Database Error: {0}".format(error))
-    return record
-
-db_insert = ("INSERT INTO `%s` (`clid`, `%s`) VALUES ('%s', '%s')")
-db_update = ("UPDATE `%s` SET `%s` = '%s' WHERE did = '%s'")
-#
-# Changed to DID
-#
 
 agi = AGI()
 agi.answer()
 
 clid = agi.env['agi_accountcode']
 
-# Asterisk Dial-plan Application 'DumpChan()'
-#Variables:
-#WOMBAT_HOPPER_ID=2145573608
-#warlist=38418
-#NUM=
-#SIPCALLID=1583cd9c69daeca70f5a91477e22f3b7@172.17.70.223:5060
-
-wombat = agi.get_variable('WOMBAT_HOPPER_ID')
-#warlist = agi.get_variable('agi_accountcode')
-newTable = agi.get_variable('table')
-warlist = agi.env['agi_accountcode']
-#
-# Changed to DID
-#
-
-
-agi.verbose("Database Record: {0}".format(warlist))
-## -broken- data_insert(db_update % ('timestamp', 'now()', warlist))
-
-
-amdstatus = agi.env['agi_arg_2']
-amdreason = agi.env['agi_arg_3']
-
-
-
-if amdstatus == "MACHINE":
-    agi.appexec('UserEvent', 'CALLSTATUS, UniqueID:%s,V:AMD' % wombat)
-    data_insert(db_update % (newTable, 'note', '%s:%s' % (amdstatus, amdreason), warlist))
-    agi.hangup()
-
-data_insert(db_update % (newTable, 'note', '%s:%s' % (amdstatus, amdreason), warlist))
-
-agi.stream_file('wardial/cust2-start')
-
-q1 = question('wardial/cust2-q1', '12')
-data_insert(db_update % (newTable, 'q1', q1, warlist))
-
-if q1 == '1':
-
-    q2 = question('wardial/cust2-q2', '12')
-    data_insert(db_update % (newTable, 'q2', q2, warlist))
-
-    if q2 == '1':
-        q3 = question('wardial/cust2-q3', '12')
-        data_insert(db_update % (newTable, 'q3', q3, warlist))
-
-        q4 = question('wardial/cust2-q4', '12')
-        data_insert(db_update % (newTable, 'q4', q4, warlist))
-
-        q5 = question('wardial/cust2-q5', '123')
-        data_insert(db_update % (newTable, 'q5', q5, warlist))
-
-agi.stream_file('wardial/cust2-end')
+print clid
 
 agi.hangup()
-
-# calltime = agi.get_variable('ANSWEREDTIME')
-# data_insert(db_update % ('reply', calltime, warlist))
-
-
-
-
