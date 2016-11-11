773107df09312d56cae33e66d6c4bd5af546b610
poing <brian.lavallee@invite-comm.jp>
2016-11-11 19:05:51 +0900

    Added RTDocs

--- docs/conf.py
+++ docs/conf.py
@@ -15,16 +15,13 @@
 import sys
 import os
 
-import mock
- 
-MOCK_MODULES = ['ivr.ivr', 'ivr/ivr.py', 'ivr', 'ivr.py']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath("")[:-4]) 
+
 
 # -- General configuration ------------------------------------------------
 
