cf8a328778b656ec0421fbe76a7f638672a8c013
poing <brian.lavallee@invite-comm.jp>
2016-11-14 17:27:46 +0900

    DocStrings Test

--- ivr/ivr.py
+++ ivr/ivr.py
@@ -28,7 +28,24 @@ import ConfigParser
 
 
 class IVR:
-    """A simple example class"""
+    """Example function with types documented in the docstring.
+
+    `PEP 484`_ type annotations are supported. If attribute, parameter, and
+    return types are annotated according to `PEP 484`_, they do not need to be
+    included in the docstring:
+
+    Args:
+        param1 (int): The first parameter.
+        param2 (str): The second parameter.
+
+    Returns:
+        bool: The return value. True for success, False otherwise.
+
+    .. _PEP 484:
+        https://www.python.org/dev/peps/pep-0484/
+
+    """
+    
     def __init__(self, config='settings.conf'):
         """
         Load the configuration.
