915bb11a666e299a4b9a51feba711566809cd563
poing <brian.lavallee@invite-comm.jp>
2016-11-15 11:48:05 +0900

    DB Connection

--- ivr/connection.py
+++ ivr/connection.py
@@ -17,7 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-"""Example Google style docstrings.
+"""
+******************************************************************
+Example Google style docstrings.
 ******************************************************************
 
 This module demonstrates documentation as specified by the `Google Python
