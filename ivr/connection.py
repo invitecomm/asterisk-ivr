d7f9444bd58481e4fdcb3bb59cab9040f68c992c
poing <brian.lavallee@invite-comm.jp>
2016-11-15 14:18:10 +0900

    DB Connection

--- ivr/connection.py
+++ ivr/connection.py
@@ -74,12 +74,12 @@ import ConfigParser
 settings = ConfigParser.RawConfigParser()
 settings.read(os.path.join(asterisk_path,asterisk_conf))
 
-config = [{
-  'user' : settings.get(context, 'dbuser'),
-  'password' : settings.get(context, 'dbpass'),
-  'host' : settings.get(context, 'dbhost'),
-  'database' : settings.get(context, 'dbhost'),
-  'raise_on_warnings' : True,
-}]
+#config = [{
+##  'user' : settings.get(context, 'dbuser'),
+#  'password' : settings.get(context, 'dbpass'),
+#  'host' : settings.get(context, 'dbhost'),
+#  'database' : settings.get(context, 'dbhost'),
+#  'raise_on_warnings' : True,
+#}]
 
-print config
+#print config
