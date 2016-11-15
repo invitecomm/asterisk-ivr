cd9db8649a363a27d733583ea12ee2db4e52b3f1
poing <brian.lavallee@invite-comm.jp>
2016-11-15 11:40:51 +0900

    DB Connection

--- ivr/connection.py
+++ ivr/connection.py
@@ -1,60 +1,44 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim: set et sw=4 fenc=utf-8:
-#
-# Copyright 2016 INVITE Communications Co., Ltd. All Rights Reserved.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+"""Example Google style docstrings.
 
-"""Database Connection Settings from Asterisk.
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
 
-This module is used to read the database settings from an Asterisk configuration file.  It uses the Python ConfigParser to extract the setting values from the specified section (context) of the Asterisk configuration file.
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
 
 """
 
-asterisk_path = '/etc/asterisk'
+module_level_variable1 = 12345
+
+module_level_variable2 = 98765
 """int: Module level variable documented inline.
-
-The path to your asterisk configuration files.
-"""
-
-asterisk_conf = 'res_config_mysql.conf'
-"""int: The configuration file you want to use.
 
 The docstring may span multiple lines. The type may optionally be specified
 on the first line, separated by a colon.
 """
 
-context = 'general'
-"""int: Module level variable documented inline.
-
-The docstring may span multiple lines. The type may optionally be specified
-on the first line, separated by a colon.
-"""
-
-
-import os
-import ConfigParser
-
-settings = ConfigParser.RawConfigParser()
-settings.read('/etc/asterisk/res_config_mysql.conf')
-
-config = {
-  'user': settings.get('general', 'dbuser'),
-  'password': settings.get('general', 'dbpass'),
-  'host': settings.get('general', 'dbhost'),
-  'database': 'kaos_portal_live',
-  'raise_on_warnings': True,
-}
