8490f9a837bb0c1d9510a187d92dbd414a8586da
poing <brian.lavallee@invite-comm.jp>
2016-11-19 00:56:26 +0900

    basic sample

--- ivr/basic.py
+++ ivr/basic.py
@@ -10,13 +10,12 @@ myVar = 'some data'
 A Simple Variable
 """
 
-
 agi = AGI()
 """
 Create an AGI Instance 
 """
 
-agi.answer()
-"""
-I/O Communications: stdin, stdout, and stderr
-"""
+#agi.answer()
+#"""
+#I/O Communications: stdin, stdout, and stderr
+#"""
