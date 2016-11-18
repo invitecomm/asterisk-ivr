6a908ad3d95b6c2aa2bf59a32d8f365393f409d1
poing <brian.lavallee@invite-comm.jp>
2016-11-18 21:59:58 +0900

    basic sample

--- ivr/basic.py
+++ ivr/basic.py
@@ -23,6 +23,31 @@ Nothing to see here yet
 
 Need to add text here.
 """
+class ExampleError(Exception):
+    """Exceptions are documented in the same way as classes.
+
+    The __init__ method may be documented in either the class level
+    docstring, or as a docstring on the __init__ method itself.
+
+    Either form is acceptable, but the two should not be mixed. Choose one
+    convention to document the __init__ method and be consistent with it.
+
+    Note:
+        Do not include the `self` parameter in the ``Args`` section.
+
+    Args:
+        msg (str): Human readable string describing the exception.
+        code (:obj:`int`, optional): Error code.
+
+    Attributes:
+        msg (str): Human readable string describing the exception.
+        code (int): Exception error code.
+
+    """
+
+    def __init__(self, msg, code):
+        self.msg = msg
+        self.code = code
 
 try:
     """
