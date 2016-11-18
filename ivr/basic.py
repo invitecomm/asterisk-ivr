38418ef53b68b9a9250549b18ff9a277b97689fd
poing <brian.lavallee@invite-comm.jp>
2016-11-18 22:08:45 +0900

    basic sample

--- ivr/basic.py
+++ ivr/basic.py
@@ -18,43 +18,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 """
-Asterisk Configuration File
+Basic Sample
 ######################################################
-This module is used to read the database settings from an Asterisk configuration file.  It uses the Python ConfigParser to load the setting values directly from the specified section (context) of the Asterisk configuration file.
-
-.. note:: You need the `MySQL Connector/Python <https://dev.mysql.com/downloads/connector/python/>`_ installed on your system to connect to the database.
-
-Example:
-    Import the module, get the config settings, and pass them to your mysql connection::
-
-        import ivr.connection
-        config = ivr.connection.config
-        database = mysql.connect(**config)
+Nothing to see here yet!
 
 """
 
-class ExampleError(Exception):
-    """Exceptions are documented in the same way as classes.
+from asterisk.agi import *
+from distutils.util import strtobool
 
-    The __init__ method may be documented in either the class level
-    docstring, or as a docstring on the __init__ method itself.
-
-    Either form is acceptable, but the two should not be mixed. Choose one
-    convention to document the __init__ method and be consistent with it.
-
-    Note:
-        Do not include the `self` parameter in the ``Args`` section.
-
-    Args:
-        msg (str): Human readable string describing the exception.
-        code (:obj:`int`, optional): Error code.
-
-    Attributes:
-        msg (str): Human readable string describing the exception.
-        code (int): Exception error code.
-
-    """
-
-    def __init__(self, msg, code):
-        self.msg = msg
-        self.code = code
+agi = AGI()
+agi.answer()
