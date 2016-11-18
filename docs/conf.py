888428ef19decabe130e9c94c5fe13f3d5cf9710
poing <brian.lavallee@invite-comm.jp>
2016-11-18 23:32:40 +0900

    basic sample

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['asterisk.agi', 'asterisk.agi.AGI']
+MOCK_MODULES = ['asterisk.agi', 'asterisk.agi.AGI', 'AGI']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
    
 # If extensions (or modules to document with autodoc) are in another directory,
