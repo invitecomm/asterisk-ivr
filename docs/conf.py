8b87ee15952d6810ca715a17400230ca3e355387
poing <brian.lavallee@invite-comm.jp>
2016-11-22 13:53:19 +0900

    IVR Work

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,8 @@ import sys, os
 import mock
 #
 MOCK_MODULES = ['AGI', 'AGI.answer']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
    
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
