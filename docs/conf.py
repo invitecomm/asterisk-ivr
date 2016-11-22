ddb41be0e487db23a5c882ecede678293c8dccea
poing <brian.lavallee@invite-comm.jp>
2016-11-22 13:58:20 +0900

    IVR Work

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 import sys, os
 import mock
 #
-MOCK_MODULES = ['asterisk.agi']
+MOCK_MODULES = ['asterisk.agi', 'asterisk.agi.AGI']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
    
