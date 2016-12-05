5b143f523f2a0cda7e233e22e5e3da8ef2bb8538
poing <brian.lavallee@invite-comm.jp>
2016-12-05 10:38:12 +0900

    testing

--- pigeonhole/survey.py
+++ pigeonhole/survey.py
@@ -36,7 +36,7 @@ sys.setdefaultencoding("utf-8")
 from datetime import date, datetime, timedelta
 import mysql.connector as mariadb
 
-config = ivr.connection.config()
+config = ivr.connection.config('portal')
 results = ivr.connection.config('wardial')
 
 def database(query):                           
