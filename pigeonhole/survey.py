3762dc5eae00800207a83804d99e2c4fef85f7e1
poing <brian.lavallee@invite-comm.jp>
2016-12-05 10:41:56 +0900

    testing

--- pigeonhole/survey.py
+++ pigeonhole/survey.py
@@ -212,6 +212,11 @@ listData.pop()  # Remove project_next from end of list
 #env = agi.env['agi_arg_1']
 #agi.appexec('DumpChan')
 
+agi.verbose('Playback: {0}'.format((project_start)))    
+prompt(project_next)    
+agi.verbose('Playback: {0}'.format((project_finish)))
+agi.verbose('Done')
+
 agi.stream_file('tt-monty-knights')
 
 agi.hangup()
